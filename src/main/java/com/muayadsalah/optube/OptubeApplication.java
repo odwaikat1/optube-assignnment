f1bb2c989a6ed8265d24e5b765d26d1f8781a198
Muayad Salah <msalah@apple.com>
2021-09-04 16:29:52 +0300

    Generate spring boot starter app

---
+++ src/main/java/com/muayadsalah/optube/OptubeApplication.java
@@ -0,0 +1,13 @@
+package com.muayadsalah.optube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muayadsalah/optube/OptubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muayadsalah.optube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
